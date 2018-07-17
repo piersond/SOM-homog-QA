12c460ed6ca29b0eafb38c6394d75790dbb1401a
piersond <drk.pierson@gmail.com>
2018-07-17 10:52:38 -0700

    Created HomogQA_example.R
    Example script to both homog and QA data in a SOM Gdrive folder

---
+++ HomogQA_example.R
@@ -0,0 +1,26 @@
+devtools::install_github("srearl/soilHarmonization")
+
+library('soilHarmonization')
+
+# Set working directory
+setwd("C:/Users/Derek/Google Drive/Code/R")
+
+# Note: Make sure the homogenization QA script file is in your working directory
+source("SoilHomog_QA.R")
+
+### User input ###
+###############################################################
+
+# Input LTER-SOM folder name to homogenize 
+GD.folder <- 'UMBS_DIRT_Bulk_Den_2004_2009'
+
+# Local temporary directory address
+temp.files <- "C:/Users/Derek/Google Drive/Code/R/temp"
+###############################################################
+
+# Data homogenization
+data_homogenization(directoryName = GD.folder, 
+                    temporaryDirectory = temp.files)
+
+# Homogenization QA
+homogenization.qa(directoryName=GD.folder, temp.folder=temp.files)
