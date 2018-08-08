a1896b50673c927730190d8f3e728ef28bb13bd3
piersond <drk.pierson@gmail.com>
2018-08-08 15:16:27 -0700

    Updated code for Check 1 and Check 3
    Should get fewer false negative FLAGS now.

--- SoilHomog_QA.R
+++ SoilHomog_QA.R
@@ -41,7 +41,8 @@ homogenization.qa <- function(directoryName, temp.folder) {
   dirFileNames <- dirFileNames[dirFileNames != 'duplicates']
   
   # Load raw data
-  raw.name <- dirFileNames[!grepl("key",dirFileNames, ignore.case = T) & !grepl("HMGZD",dirFileNames, ignore.case = T)]
+  raw.name <- dirFileNames[!grepl("key",dirFileNames, ignore.case = T) & !grepl("HMGZD",dirFileNames, ignore.case = T) & 
+                             !grepl("log.txt",dirFileNames, ignore.case = T) & !grepl("plot.png",dirFileNames, ignore.case = T)]
   raw.data <- gs_read(ss=gs_title(raw.name), ws = 1, skip=0)
   
   # Load keykey
@@ -76,7 +77,7 @@ homogenization.qa <- function(directoryName, temp.folder) {
   loc.values <- c(na.omit(key.loc[,1]))
   hmg.toprow <- c(na.omit(t(hmg.data[1,])))
   
-  if(length(intersect(loc.values[[1]], hmg.toprow))+3 == length(loc.values[[1]])) {
+  if(length(intersect(loc.values[[1]], hmg.toprow)) == length(unique(loc.values[[1]]))) {
     print("CHECK TRUE - All location data found in HMGZD")
     QA.results <- append(QA.results, "Check True - All location data found in HMGZD")
   } else {
@@ -90,28 +91,7 @@ homogenization.qa <- function(directoryName, temp.folder) {
   # CHECK 3
   # Check profile data length
   
-  ## Right now this mostly does the same check as the first check above, 
-  # but I think we may be able to expand on this code check with some further thought
-  
-  # Output number of homogenized profile data columns
-  prof.vars <- as.list(na.omit(key.prof[,c(1,4)]))
-  QA.results <- append(QA.results, c(paste0("Found ",length(prof.vars[[1]]), " profile data columns to homogenize"),""))  
-  
-  
-  for (i in 1:length(prof.vars[[1]])) {
-    if(length(na.omit(hmg.data[[prof.vars[[2]][i]]])) != length(na.omit(raw.data[[prof.vars[[1]][i]]]))) {
-      print(paste0("Raw data variable ",prof.vars[[1]][i]," does not match HMGZD variable ",prof.vars[[2]][i]))
-      QA.results <- append(QA.results, paste0("FLAG -Raw data variable ",prof.vars[[1]][i]," does NOT match HMGZD variable ",prof.vars[[2]][i]))
-    } else {
-      print(paste0("Raw data variable ",prof.vars[[1]][i]," confirmed to ",prof.vars[[2]][i]))
-      QA.results <- append(QA.results, paste0("Raw data variable ",prof.vars[[1]][i]," confirmed to ",prof.vars[[2]][i]))
-    }
-  }
-  # Console print status
-  print("Variable input checks complete")
-  
-  ### Data QA plots ###
-  # hmg.data housekeeping
+  # dataframe housekeeping
   # Convert dplyr dataframe output to standard dataframe
   hmg.data <- as.data.frame(hmg.data)
   key.prof <- as.data.frame(key.prof)
@@ -124,6 +104,27 @@ homogenization.qa <- function(directoryName, temp.folder) {
   hmg.data[hmg.data==nay.1]<-NA
   hmg.data[hmg.data==nay.2]<-NA
   
+  raw.data[raw.data==nay.1]<-NA
+  raw.data[raw.data==nay.2]<-NA
+  
+  # Output number of homogenized profile data columns
+  prof.vars <- as.list(na.omit(key.prof[,c(1,4)]))
+  QA.results <- append(QA.results, c(paste0("Found ",length(prof.vars[[1]]), " profile data columns to homogenize"),""))  
+  
+  
+  for (i in 1:length(prof.vars[[1]])) {
+    if(length(na.omit(hmg.data[[prof.vars[[2]][i]]])) != length(na.omit(raw.data[[prof.vars[[1]][i]]]))) {
+      print(paste0("FLAG -Raw data variable ",prof.vars[[1]][i]," does not match HMGZD variable ",prof.vars[[2]][i]))
+      QA.results <- append(QA.results, paste0("FLAG -Raw data variable ",prof.vars[[1]][i]," does NOT match HMGZD variable ",prof.vars[[2]][i]))
+    } else {
+      print(paste0("Raw data variable ",prof.vars[[1]][i]," confirmed to ",prof.vars[[2]][i]))
+      QA.results <- append(QA.results, paste0("Raw data variable ",prof.vars[[1]][i]," confirmed to ",prof.vars[[2]][i]))
+    }
+  }
+  # Console print status
+  print("Variable input checks complete")
+  
+  ### Data QA plots ###
   # Prep values and data for plotting
   # Find number of experiment levels and number of data columns
   exp.lvls <- length(na.omit((key.prof[1:13,1])))
